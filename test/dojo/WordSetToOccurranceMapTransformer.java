78723b8dd6d355ba34703273adb16f1bbb92b8fb
arkangelofkaos <arkangelofkaos@gmail.com>
2014-03-17 21:39:49 +0000

    dojo complete

--- test/dojo/WordSetToOccurranceMapTransformer.java
+++ test/dojo/WordSetToOccurranceMapTransformer.java
@@ -21,25 +21,26 @@ import static org.hamcrest.Matchers.contains;
 public class WordSetToOccurranceMapTransformer {
 
     @Test
-    public void givenLine10_andWordSetFooBar_returnMapKeysFooAndBarToValues10() throws Exception {
+    public void flipMapOfLineNumberToWords() throws Exception {
         Map<Long, Set<Word>> wordSetMap = new HashMap<>();
         wordSetMap.put(10L, new HashSet<>(asList(new Word("foo"), new Word("bar"))));
 
-        Map<Word, List<Long>> map = transform(wordSetMap);
+        Map<Word, List<Long>> map = flipMap(wordSetMap);
         assertThat(map.get(new Word("foo")), contains(10L));
     }
 
-    private Map<Word, List<Long>> transform(Map<Long, Set<Word>> wordSetMap) {
-        SortedMap<Word, List<Long>> result = new TreeMap<>();
+    private Map<Word, List<Long>> flipMap(Map<Long, Set<Word>> wordSetMap) {
+        SortedMap<Word, List<Long>> wordToLineNumberMap = new TreeMap<>();
 
-        for (Map.Entry<Long, Set<Word>> longSetEntry : wordSetMap.entrySet()) {
-            for (Word word : longSetEntry.getValue()) {
-                List<Long> list = result.getOrDefault(word, new LinkedList<>());
-                list.add(longSetEntry.getKey());
-                result.put(word, list);
+        for (Map.Entry<Long, Set<Word>> lineNumberToWords : wordSetMap.entrySet()) {
+            for (Word word : lineNumberToWords.getValue()) {
+                List<Long> list = wordToLineNumberMap.getOrDefault(word, new LinkedList<>());
+                list.add(lineNumberToWords.getKey());
+
+                wordToLineNumberMap.put(word, list);
             }
         }
 
-        return result;
+        return wordToLineNumberMap;
     }
 }
