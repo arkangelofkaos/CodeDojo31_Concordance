6025360df9c9472a68498217d18503086be0c03e
arkangelofkaos <arkangelofkaos@gmail.com>
2014-03-18 00:38:21 +0000

    Failing file reader test

---
+++ src/dojo/concordance/io/FileReader.java
@@ -0,0 +1,16 @@
+package dojo.concordance.io;
+
+import dojo.concordance.Line;
+
+import java.util.List;
+
+/**
+ * @author arkangelofkaos
+ */
+public class FileReader {
+
+    public List<Line> read(String file) {
+        return null;
+    }
+
+}
---
+++ test/dojo/concordance/io/FileReaderTest.java
@@ -0,0 +1,22 @@
+package dojo.concordance.io;
+
+import dojo.concordance.Line;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * @author arkangelofkaos
+ */
+public class FileReaderTest {
+
+    @Test(groups = "unit")
+    public void readingConcordanceFile_returnsSixLines() throws Exception {
+        FileReader fileReader = new FileReader();
+        List<Line> lines = fileReader.read("/concordance_test.txt");
+        assertThat(lines.size(), is(6));
+    }
+}
